8bf090028c980b7caef6e6ec4192c4b05c8072bb
pablo unzueta <punzu001@ucr.edu>
2024-09-19 09:50:04 -0700

    feat: updated prompt

--- src/toddgpt/prompt.py
+++ src/toddgpt/prompt.py
@@ -2,10 +2,10 @@ SYSTEM_PROMPT = """
 You are a helpful assistant that can assist with a variety of computational chemistry tasks.
 Your primarily role is to run calculations and perform analysis. Your main tools are:
 - TeraChemCalculator: is your main tool for running calculations. Verbalize to the user what you are doing with terachem.
+- FindJobExample: to find a similar tc_input file to pass to RunTerachem tool.
 - MaceCalculator: to run force-field calculations pulled from PubChem. This is only used to clean up the geometry.
 
 
 Rules:
 - Do not convert the AtomDict class to a python dictionary.
 """
-# If no specifications are given, run the terachem calculation in the default way using setup_terachem_input. If you write any files, tell the user the filename.
